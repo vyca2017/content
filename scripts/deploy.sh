cd4836bc243f4fc22ed00608192d45be65b5a2bc
Johannes Schickling <schickling.j@gmail.com>
2017-02-01 19:03:56 +0100

    better env vars

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,22 @@
 
 set -e
 
+if [ ! -z "$CIRCLE_BRANCH" ]; then
+  UPPER_BRANCH=$(echo $CIRCLE_BRANCH | tr '[a-z]' '[A-Z]')
+  GRAPHCOOL_PAT_FROM_BRANCH="GRAPHCOOL_PAT_${UPPER_BRANCH}"
+  GRAPHCOOL_PROJECT_ID_FROM_BRANCH="GRAPHCOOL_PROJECT_ID_${UPPER_BRANCH}"
+  GRAPHCOOL_PAT=${!GRAPHCOOL_PAT_FROM_BRANCH:?$GRAPHCOOL_PAT_FROM_BRANCH env var not set}
+  GRAPHCOOL_PROJECT_ID=${!GRAPHCOOL_PROJECT_ID_FROM_BRANCH:?$GRAPHCOOL_PROJECT_ID_FROM_BRANCH env var not set}
+fi
+
+
+BRANCH="${CIRCLE_BRANCH:-dev}"
+
+GRAPHCOOL_PAT="${GRAPHCOOL_PAT:?GRAPHCOOL_PAT env variable not set}"
+GRAPHCOOL_PROJECT_ID="${GRAPHCOOL_PROJECT_ID:?GRAPHCOOL_PROJECT_ID env variable not set}"
+CIRCLE_TOKEN="${CIRCLE_TOKEN:?CIRCLE_TOKEN env variable not set}"
+
 markdown-to-graphcool -c content --reset 
 
 # trigger rebuild of homepage
-curl -X POST https://circleci.com/api/v1.1/project/github/graphcool/homepage/tree/$CIRCLE_BRANCH?circle-token=$CIRCLE_TOKEN
+curl -X POST https://circleci.com/api/v1.1/project/github/graphcool/homepage/tree/$BRANCH?circle-token=$CIRCLE_TOKEN
