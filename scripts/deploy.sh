0bc7c9b73cb480ed15b2051c7f7013a5f90df380
Johannes Schickling <schickling.j@gmail.com>
2017-02-01 16:53:35 +0100

    added circle token

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,4 +5,4 @@ set -e
 markdown-to-graphcool -c content --reset 
 
 # trigger rebuild of homepage
-curl -X POST https://circleci.com/api/v1.1/project/github/graphcool/homepage/tree/$CIRCLE_BRANCH
+curl -X POST https://circleci.com/api/v1.1/project/github/graphcool/homepage/tree/$CIRCLE_BRANCH?circle-token=$CIRCLE_TOKEN
